1eb8655baaad2204c57c96480d70bb76c35e95a6
gopherstein <spankenstien@gmail.com>
2016-05-09 14:02:00 -0500

    Bug Fixes

--- aws/1.1.7/security_groups.tf
+++ aws/1.1.7/security_groups.tf
@@ -1,4 +1,38 @@
+resource "aws_security_group" "elb_sg" {
+    name = "${var.cluster_id}_elb_sg"
+    description = "Allow traffic to pass over any port internal to the VPC"
+
+    ingress {
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    egress {
+        from_port = 30000
+        to_port = 40000
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    egress {
+        from_port = 10250
+        to_port = 10250
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    vpc_id = "${aws_vpc.kube_cluster.id}"
+
+    tags {
+        KubernetesCluster = "${var.cluster_id}"
+    }
+}
+
+
 resource "aws_security_group" "kubernetes_sg" {
+    depends_on = ["aws_security_group.elb_sg"]
     name = "${var.cluster_id}_sg"
     description = "Allow traffic to pass over any port internal to the VPC"
 
@@ -41,38 +75,6 @@ resource "aws_security_group" "kubernetes_sg" {
         to_port = 10250
         protocol = "tcp"
         cidr_blocks = ["${aws_security_group.elb_sg.id}"]
-    }
-
-    vpc_id = "${aws_vpc.kube_cluster.id}"
-
-    tags {
-        KubernetesCluster = "${var.cluster_id}"
-    }
-}
-
-resource "aws_security_group" "elb_sg" {
-    name = "${var.cluster_id}_elb_sg"
-    description = "Allow traffic to pass over any port internal to the VPC"
-
-    ingress {
-        from_port = 0
-        to_port = 0
-        protocol = "-1"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
-
-    egress {
-        from_port = 30000
-        to_port = 40000
-        protocol = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
-
-    egress {
-        from_port = 10250
-        to_port = 10250
-        protocol = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
     }
 
     vpc_id = "${aws_vpc.kube_cluster.id}"
